28e484ed5e39bb7d5d35e39c5c5b786f3ee5bd31
青峰 <haifeng.jiang@mihoyo.com>
2021-03-16 18:58:27 +0800

    feat: add ThreadPoolExecutor

---
+++ threading/thread.go
@@ -0,0 +1,59 @@
+package threading
+
+import (
+	"github.com/hackfengJam/gokit/future"
+	"github.com/hackfengJam/gokit/isafe"
+)
+
+// ThreadPoolExecutor Thread Pool Executor
+type ThreadPoolExecutor struct {
+	workerChan  chan future.PlaceholderType
+	requestChan chan taskFunc
+}
+
+type taskFunc func()
+
+// NewThreadPoolExecutor New ThreadPoolExecutor
+func NewThreadPoolExecutor(maxWorkers int, maxWaiters int) *ThreadPoolExecutor {
+	return &ThreadPoolExecutor{
+		workerChan:  make(chan future.PlaceholderType, maxWorkers),
+		requestChan: make(chan taskFunc, maxWaiters),
+	}
+}
+
+// Submit submit
+func (tpe *ThreadPoolExecutor) Submit(task taskFunc) {
+	tpe.requestChan <- task
+}
+
+// workerReady Worker Ready
+func (tpe *ThreadPoolExecutor) workerReady() {
+	<-tpe.workerChan
+}
+
+// Run run
+func (tpe *ThreadPoolExecutor) Run() {
+	go func() {
+		var taskQueue []taskFunc
+		for {
+			var req taskFunc
+			select {
+			case req = <-tpe.requestChan:
+				taskQueue = append(taskQueue, req)
+			case tpe.workerChan <- future.Placeholder:
+				if len(taskQueue) == 0 {
+					tpe.workerReady()
+				} else {
+					task := taskQueue[0]
+					taskQueue = taskQueue[1:]
+					go func() {
+						defer isafe.Recover(func() {
+							tpe.workerReady()
+						})
+						task()
+					}()
+				}
+			}
+		}
+	}()
+}
---
+++ threading/thread_test.go
@@ -0,0 +1,37 @@
+package threading
+
+import (
+	"runtime"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestThreadPoolExecutor(t *testing.T) {
+	times := 1000
+	pool := NewThreadPoolExecutor(runtime.NumCPU(), 10000000)
+	// pool := NewThreadPoolExecutor(3, 10000000)
+
+	// start := time.Now()
+	var counter int32
+	var waitGroup sync.WaitGroup
+	for i := 0; i < times; i++ {
+		waitGroup.Add(1)
+		pool.Submit(func() {
+			time.Sleep(time.Millisecond)
+			atomic.AddInt32(&counter, 1)
+			waitGroup.Done()
+		})
+		// fmt.Println(fmt.Sprintf("th: %d, has been submitted", i))
+	}
+	pool.Run()
+
+	// fmt.Println("submit finished.")
+	waitGroup.Wait()
+	// fmt.Println(fmt.Sprintf("elapsed: %v", time.Now().Sub(start)))
+
+	assert.Equal(t, times, int(counter))
+}
