3193c6bcf5cfde08f0a281e3a49e003924cc1ecb
青峰 <haifeng.jiang@mihoyo.com>
2021-09-08 19:18:04 +0800

    feat: add example

--- ipoolmap/pool_map.go
+++ ipoolmap/pool_map.go
@@ -28,6 +28,44 @@ var (
 )
 
 // PoolMap Pool Map
+/*
+example:
+	type TestResult {
+        UID int64
+	}
+	args := []int64{1,2,3}
+
+	// 定义执行函数
+	funcs := make([]func() (interface{}, error), 0)
+	for _, v := range args {
+		uid := v
+		funcs = append(funcs, func() (interface{}, error) {
+			// do something
+			item := &TestResult{}
+			item.UID = uid
+			return item, nil
+		})
+	}
+
+	// 并发执行&等待所有结果
+	results, err := ipoolmap.PoolMap(ctx, funcs)
+	if err != nil {
+		// log
+		return
+	}
+
+	// 获取结果
+	for _, result := range results {
+		if result.Err != nil {
+			continue
+		}
+		item, ok := result.Data.(*TestResult)
+		if !ok {
+			continue
+		}
+		resultList = append(resultList, item)
+	}
+*/
 func PoolMap(ctx context.Context, funcs []func() (interface{}, error)) ([]PoolResult, error) {
 	var wg sync.WaitGroup
 	wg.Add(len(funcs))
